d6f363707d5dde1dc6e4565e21998f600bf682d9
Supriyankumar <89706.kumar@students.itu.edu>
2019-08-12 11:28:52 -0700

    functions code challenge
    This is a code challenge for the functions section from codecademy

---
+++ codechallenge_functions.py
@@ -0,0 +1,12 @@
+#Codecademy code challenge usinf functions.
+
+def lots_of_math(a,b,c,d):
+    e = a+b
+    f = d-c
+    print(e)
+    print(f)
+    g= e*f
+    print(g)
+    return g%a
+print(lots_of_math(1,2,3,4))
+print(lots_of_math(1,1,1,1))
