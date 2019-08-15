f73adfcd74664651deffd73f2d1fde08637a057a
Supriyankumar <89706.kumar@students.itu.edu>
2019-08-15 16:56:19 -0700

    adding counter

--- divisiblebyten.py
+++ divisiblebyten.py
@@ -1,6 +1,10 @@
 # check the list of numbers that is divisible by ten and return that number divisible by ten
 
 def divisible_by_ten(nums):
+    count = 0
     for num in nums:
         if num%10 == 0:
+            count+= 1
             return num
+
+print(divisible_by_ten([print(divisible_by_ten([20, 25, 30, 35, 40]))]))
