335a39a5172810b026a00b314068edfa6b0495d8
Supriyankumar <89706.kumar@students.itu.edu>
2019-08-15 17:01:06 -0700

    resolved return error

--- divisiblebyten.py
+++ divisiblebyten.py
@@ -2,9 +2,9 @@
 
 def divisible_by_ten(nums):
     count = 0
-    for num in nums:
-        if num%10 == 0:
-            count+= 1
-            return num
+    for number in nums:
+        if (number % 10 == 0):
+            count += 1
+    return count
 
 print(divisible_by_ten([print(divisible_by_ten([20, 25, 30, 35, 40]))]))
