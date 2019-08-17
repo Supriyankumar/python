f11af38437f41fa5a540685cc200f826140f4ec4
Supriyankumar <89706.kumar@students.itu.edu>
2019-08-17 10:15:59 -0700

    checking if the number is present

---
+++ deleting_starting_evennumbers.py
@@ -0,0 +1,13 @@
+# To delete the elements from the front of the list until the front of the list is not even
+
+def deleting_starting_evens(lst):
+
+  while (len(lst) > 0):
+
+
+
+
+
+
+
+print(deleting_starting_evens([4, 8, 10, 11, 12, 15]))
