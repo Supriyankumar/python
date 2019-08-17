bd6ee82dda4f69492ee87d73fcd7f6212edcc312
Supriyankumar <89706.kumar@students.itu.edu>
2019-08-17 10:21:42 -0700

    checking for even number

--- deleting_starting_evennumbers.py
+++ deleting_starting_evennumbers.py
@@ -2,12 +2,10 @@
 
 def deleting_starting_evens(lst):
 
-  while (len(lst) > 0):
-
-
-
-
-
+  while (len(lst) > 0 and lst[0] % 2 == 0):
+      lst = lst[1:]
+      return lst
 
 
 print(deleting_starting_evens([4, 8, 10, 11, 12, 15]))
+print(deleting_starting_evens([4, 8, 10]))
