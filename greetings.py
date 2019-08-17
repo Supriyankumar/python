2545c08ad8b5112b02a71854ddb519b5e35be6df
Supriyankumar <89706.kumar@students.itu.edu>
2019-08-16 22:26:56 -0700

    appending and returning empty list values

--- greetings.py
+++ greetings.py
@@ -2,5 +2,7 @@ def add_greetings(names):
     empty_list = []
     for name in names:
         empty_list.append("Hello, " + name)
-    print(empty_list)
-    
+    return(empty_list)
+
+
+print(add_greetings(["Owen", "Max", "Sophie"]))
